3bbb8acac434fb66ff70f5e97c39424edc6fc2bd
Wimpje <wimhoringa@gmail.com>
2019-07-19 16:43:57 +0200

    make it a bit nicer

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,36 @@
 <script>
-	export let whatis;
-	export let count = 0;
-   function handleWhy() {
-		whatis = "let me tell you a story"
-	}
-   function handleKnowItAll() {
-		whatis = "great, that saves me a lot of explanation";
-	}
-   
+  import { fade } from "svelte/transition";
+
+  export let whatis;
+  export let showButtons = true;
+  function handleWhy() {
+    whatis = "come back in a month, then I'll tell you a story";
+    showButtons = false;
+  }
+  function handleKnowItAll() {
+    whatis = "great, that saves me a lot of explanation";
+    showButtons = false;
+  }
 </script>
 
 <style>
-	
+.center {
+   display:flex;
+   justify-content:center;
+   align-items:center;
+}
 </style>
-<img src="logo.png" alt="Site logo"/>
-<h1>{whatis}</h1>
-<button on:click={handleWhy}>why not?</button>
-<button on:click={handleKnowItAll}>i know</button>
+<div class="center">
+   <img src="logo.png" alt="Site logo" />
+</div>
+<h1 class="center">{whatis}</h1>
+{#if showButtons}
+  <div class="center choose-wisely" transition:fade={{ delay: 0, duration: 300 }}>
+    <button on:click={handleWhy}>why not?</button>
+    <button on:click={handleKnowItAll}>i know</button>
+  </div>
+{:else}
+  <div class="center" transition:fade={{ delay: 300, duration: 300 }}>
+     this page is not done yet. come back later to be enlightened.
+  </div>
+{/if}
